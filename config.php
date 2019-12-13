72c2757c1173843bdf879ced389f3bc89596f485
Muhammad Risky <56730687+riskyfreak@users.noreply.github.com>
2019-12-13 17:34:35 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?
 
-$useragent = "xxxxxxx";
-$token = "xxxxxxx";
-$PHPSESSID = "PHPSESSID=xxxxxxx";
-$cfduid = "__cfduid=xxxxxxxxx";
+$useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:71.0) Gecko/20100101 Firefox/71.0";
+$token = "2f5bc4d88fd5c0dd5750a67b74a9424e04451dbe";
+$PHPSESSID = "PHPSESSID=akiuod5k81i9otsretlfr38keq";
+$cfduid = "__cfduid=ddf4870fe179b6de1755ff58c0bdbe0fc1576229286";
